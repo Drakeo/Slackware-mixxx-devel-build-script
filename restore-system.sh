6adabd47940e41131a767a4b1f2802f87586bec9
drakeo <flats_fixed@flatsfixedbicycles.com>
2021-08-19 16:24:53 -0500

    add a restore script to uninstall  libraries and revert  back to stok alsa-plugins and pulseaudio

---
+++ restore-system.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+removepkg audacity
+removepkg chromaprint
+removepkg faad2
+removepkg google-apputils
+removepkg hidapi
+removepkg jack2
+removepkg ladspa_sdk
+removepkg libebur128 
+removepkg libmp4v2
+removepkg libopenmpt 
+removepkg libopenmpt-modplug
+removepkg libshout
+removepkg lilv
+removepkg lv2
+removepkg mixxx
+removepkg mxml 
+removepkg portaudio 
+removepkg portmidi  
+removepkg protobuf
+removepkg python-dateutil
+removepkg python-gflags
+removepkg pytz
+removepkg qjackctl
+removepkg QUSB
+removepkg rubberband
+removepkg serd
+removepkg set_rlimits
+removepkg sord  
+removepkg soundtouch
+removepkg sratom    
+removepkg vamp-plugin-sdk  
+# reinstall the  default slackware packages.
+slackpkg reinstall alsa-plugins 
+slackpkg reinstall pulseaudio 
+
+ 
